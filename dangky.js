eae7e9a10a5c57464e04bce9ad39cb528caeeae9
Tiendat2K2 <109364019+Tiendat2K2@users.noreply.github.com>
2023-05-26 19:52:15 +0700

    Update dangky.js

--- dangky.js
+++ dangky.js
@@ -1,37 +1,41 @@
-// JavaScript code
 function validateForm() {
-    var username = document.getElementById("username").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirmPassword").value;
-    
-    if (username === "") {
-      alert("Vui lòng nhập Username");
-      return;
-    }
-    
-    if (email === "") {
-      alert("Vui lòng nhập Email");
-      return;
-    }
-    
-    if (password === "") {
-      alert("Vui lòng nhập mật khẩu");
-      return;
-    }
-    
-    if (password !== confirmPassword) {
-      alert("Mật khẩu không khớp. Vui lòng nhập lại");
-      return;
-    }
-    
-    // Đoạn mã sau này sẽ chuyển hướng người dùng đến "dannhap.html"
-    // Có thể thay thế bằng đoạn mã tương ứng với chức năng của bạn
-    goToHomePage();
+  var username = document.getElementById("username").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var confirmPassword = document.getElementById("confirmPassword").value;
+
+  if (username === "") {
+    alert("Vui lòng nhập Username");
+    return;
   }
-  
-  function goToHomePage() {
-    alert("Đăng ký thành công")
-    // Chuyển hướng người dùng đến "dannhap.html"
-    window.location.href = "dannhap.html";
+
+  if (email === "") {
+    alert("Vui lòng nhập Email");
+    return;
   }
+
+  if (/\s/.test(email)) {
+    alert("Email không được chứa khoảng trắng");
+    return;
+  }
+
+  if (password === "") {
+    alert("Vui lòng nhập mật khẩu");
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    alert("Mật khẩu không khớp. Vui lòng nhập lại");
+    return;
+  }
+
+  // Đoạn mã sau này sẽ chuyển hướng người dùng đến "dannhap.html"
+  // Có thể thay thế bằng đoạn mã tương ứng với chức năng của bạn
+  goToHomePage();
+}
+
+function goToHomePage() {
+  alert("Đăng ký thành công");
+  // Chuyển hướng người dùng đến "dannhap.html"
+  window.location.href = "dannhap.html";
+}
