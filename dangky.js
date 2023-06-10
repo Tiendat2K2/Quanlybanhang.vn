f23da8ac01a0aca707fb1863a206d1ee9173435b
Tiendat2K2 <109364019+Tiendat2K2@users.noreply.github.com>
2023-06-10 19:11:38 +0700

    Update dangky.js
    grfg

--- dangky.js
+++ dangky.js
@@ -3,6 +3,7 @@ function validateForm() {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
   var confirmPassword = document.getElementById("confirmPassword").value;
+  var checkbox = document.getElementById("agreeCheckbox");
 
   if (username === "") {
     alert("Vui lòng nhập Username");
@@ -19,6 +20,11 @@ function validateForm() {
     return;
   }
 
+  if (!validateEmail(email)) {
+    alert("Email không hợp lệ");
+    return;
+  }
+
   if (password === "") {
     alert("Vui lòng nhập mật khẩu");
     return;
@@ -29,9 +35,19 @@ function validateForm() {
     return;
   }
 
+  if (!checkbox.checked) {
+    alert("Vui lòng chọn 'I Agree To The Terms & Conditions'");
+    return;
+  }
+
   // Đoạn mã sau này sẽ chuyển hướng người dùng đến "dannhap.html"
   // Có thể thay thế bằng đoạn mã tương ứng với chức năng của bạn
   goToHomePage();
+}
+
+function validateEmail(email) {
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 }
 
 function goToHomePage() {
